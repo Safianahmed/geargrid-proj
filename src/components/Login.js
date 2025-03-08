d7edfbef2d870ee1cc356afb4f599dc0b21ac9a5
Safian <safian.ahmed14@gmail.com>
2025-03-08 18:12:04 -0500

    Code clean up

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../css/styles.css";
+import "../css/Login.css"; // Corrected path to Login.css
 
 const Login = () => {
   const navigate = useNavigate();
 
   const handleLogin = () => {
-    localStorage.setItem("isAuthenticated", "true"); 
-    navigate("/menu"); 
+    localStorage.setItem("isAuthenticated", "true");
+    navigate("/menu");
   };
 
   return (
@@ -18,15 +18,15 @@ const Login = () => {
           <input type="text" placeholder="Phone number, username, or email" required />
           <input type="password" placeholder="Password" required />
           <button type="button" className="auth-btn" onClick={handleLogin}>
-            Log In
+            Log in
           </button>
         </form>
-        <p>
-          <a href="/forgot-password" className="forgot-password">Forgot password?</a>
-        </p>
+        <a href="/forgot-password" className="forgot-password">
+          Forgot password?
+        </a>
       </div>
-      <div className="auth-box signup-box">
-        <p>Don't have an account? <a href="/signup">Sign up</a></p>
+      <div className="signup-box">
+        Don't have an account? <a href="/signup">Sign up</a>
       </div>
     </div>
   );
